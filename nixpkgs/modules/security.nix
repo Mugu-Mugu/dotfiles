20597a64a443f36f4dc46f09e8f08137e73ba0cc
David <david.amar@wakeo.co>
2021-12-07 01:01:24 +0100

    Add authy configuration

--- nixpkgs/modules/security.nix
+++ nixpkgs/modules/security.nix
@@ -6,4 +6,14 @@
     mutableKeys = true;
     mutableTrust = true;
   };
+
+  nixpkgs.config = {
+    permittedInsecurePackages = [
+      "electron-9.4.4"
+    ];
+  };
+
+  home.packages = with pkgs; [
+    authy
+  ];
 }

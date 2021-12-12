3ac22742101b7979c5c507d4d6e4dc20d9b7f6e6
David <david.amar@wakeo.co>
2021-12-12 17:42:58 +0100

    Add all profiles to flake configuration

--- flake.nix
+++ flake.nix
@@ -7,14 +7,23 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
-    homeConfigurations = {
-      david-bootstrap = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/david";
-        username = "david";
-        configuration.imports = [ ./nixpkgs/profiles/bootstrap.nix ];
+  outputs = inputs:
+    let makeStandardConf = profile_file: {
+          nixosModules."${profile_file}" = ./. + "/nixpkgs/profiles/${profile_file}.nix";
+          homeConfigurations.${profile_file} = inputs.home-manager.lib.homeManagerConfigurations {
+            system = "x86_64-linux";
+            homeDirectory = "/home/david";
+            username = "david";
+            configuration.imports = [ (./. +  "./nixpkgs/profiles/${profile_file}.nix") ];
+          };
+        }; in
+      # makeStandardConf("bootstrap") // makeStandardConf("base");
+      {nixosModules.bootstrap = import ./nixpkgs/profiles/bootstrap.nix;
+          homeConfigurations.bootstrap = inputs.home-manager.lib.homeManagerConfiguration {
+            system = "x86_64-linux";
+            homeDirectory = "/home/david";
+            username = "david";
+            configuration.imports = [ ./nixpkgs/profiles/bootstrap.nix ];
+          };
       };
-    };
-  };
 }

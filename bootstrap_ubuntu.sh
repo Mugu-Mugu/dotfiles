a16a6b6212c8819384621576c1f9d2ab65d040ce
MuguMugu <amardavid3241@gmail.com>
2022-01-21 20:06:29 +0000

    Add hardware utils

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 # install bootstrap dependencies
-sudo apt install curl
+sudo apt install -y git
 
-# install nix toolchain
-curl -L https://nixos.org/nix/install | sh
+(
+
+cd ~ 
+
+# ensure installation in a standardized location
+git clone https://github.com/Mugu-Mugu/dotfiles
+cd dotfiles
+# because I use ssh keys authentication
+git remote set-url origin git@github.com:Mugu-Mugu/dotfiles.git
+
+# install nix toolchain in single user
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
 . ~/.nix-profile/etc/profile.d/nix.sh
 
 # install home manager in standalone mode
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-
 nix-channel --update
-
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 nix-shell '<home-manager>' -A install
 
-# allow home manager to handle the shell configuration
-source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+# activate flake support
+nix-env -iA nixpkgs.nixUnstable
+mkdir -p ~/.config/nix/
+echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 
-# link the nix configuration to this repository
-rm -rf $HOME/.config/nixpkgs 2>/dev/null
-ln -fs $PWD/nixpkgs $HOME/.config/nixpkgs
+# home-manager is ready to handle the rest
+home-manager switch --flake ".#bootstrap"
+
+)
--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2,13 +2,16 @@
   bootstrap = { config, pkgs, ... }:
     {
       imports = [./profiles/bootstrap.nix];
+      home.file.".config/nixpkgs/home.nix".source = ./profiles/bootstrap.nix;
     };
   default = { config, pkgs, ... }:
     {
       imports = [./profiles/base.nix];
+      home.file.".config/nixpkgs/home.nix".source = ./profiles/base.nix;
     };
   wakeo = { config, pkgs, ... }:
     {
       imports = [./profiles/wakeo.nix];
+      home.file.".config/nixpkgs/home.nix".source = ./profiles/wakeo.nix;
   };
 }

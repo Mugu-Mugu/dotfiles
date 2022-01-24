81102340a351c1fd796035c6b06e351e4881b6ed
David <david.amar@wakeo.co>
2022-01-24 09:23:50 +0100

    Rollback use of firefox-wayland as it doesn't work

--- nixpkgs/modules/firefox.nix
+++ nixpkgs/modules/firefox.nix
@@ -2,24 +2,27 @@
 
 {
 
-  home.packages = with pkgs; [ firefox-wayland ];
+  home.packages = with pkgs; [ firefox ];
+
+  # Copied from nix wiki but it looks like it doesn't work
+  # home.packages = with pkgs; [ firefox-wayland ];
 
   # configuration required for screensharing
-  services.pipewire.enable = true;
-  xdg = {
-    portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-      ];
-      gtkUsePortal = true;
-    };
-  };
-  # Home Manager setup
-  home.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = 1;
-    XDG_CURRENT_DESKTOP = "sway";
-  };
+  # services.pipewire.enable = true;
+  # xdg = {
+  #   portal = {
+  #     enable = true;
+  #     extraPortals = with pkgs; [
+  #       xdg-desktop-portal-wlr
+  #       xdg-desktop-portal-gtk
+  #     ];
+  #     gtkUsePortal = true;
+  #   };
+  # };
+  # # Home Manager setup
+  # home.sessionVariables = {
+  #   MOZ_ENABLE_WAYLAND = 1;
+  #   XDG_CURRENT_DESKTOP = "sway";
+  # };
 
 }
